73acd27c81a8cf4ae23838579774f4b1adee07fb
Abner Saavedra <ingenieroabnersaavedra@gmail.com>
2025-06-10 11:28:12 -0400

    Gestión de rutas no encontradas

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,40 +1,20 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { Text, View } from '@/components/Themed';
+import{ Link, Stack} from'expo-router';
+import{ View, StyleSheet} from'react-native';
 
 export default function NotFoundScreen() {
-  return (
+    return(
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn't exist.</Text>
-
-        <Link href="/" style={styles.link}>
-          <Text style={styles.linkText}>Go to home screen!</Text>
-        </Link>
-      </View>
+        <Stack.Screen options={{ title: "Oops! This screen doesn't exist." }} />
+        <View style={styles.container}>
+        <Link href="/">Goto home screen</Link>
+        </View>
     </>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+    );
+    }
+    const styles= StyleSheet.create({
+        container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
