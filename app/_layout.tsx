f5c2c209df41f3b5b1f05e34ca04c6774bdc004f
Abner Saavedra <ingenieroabnersaavedra@gmail.com>
2025-06-10 11:18:59 -0400

    Navegación con pestañas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,6 @@ return(
         headerStyle:{backgroundColor:'#f4511e',},
         headerTintColor:'#fff',
         headerTitleStyle:{fontWeight:'bold',},}}>
-        <Stack.Screen name="(home)"/>
+        <Stack.Screen name="(tabs)"/>
     </Stack>);
 }
